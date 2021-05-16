1cfcb0fa02a25e2cb71d8fa3d2e296e6e8aec17e
root <root@NFrag-Home-Desktop.localdomain>
2021-05-16 10:08:38 -0700

    Updates

---
+++ precache-manifest.b2affbbb5e006483797d280266babb4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111d63851e907c258628193b7be2bc29",
+    "url": "/React-Quiz/index.html"
+  },
+  {
+    "revision": "e478d95cd4ed99684661",
+    "url": "/React-Quiz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/React-Quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e478d95cd4ed99684661",
+    "url": "/React-Quiz/static/js/main.2da79994.chunk.js"
+  },
+  {
+    "revision": "d617a2e1af55ff18d4a3",
+    "url": "/React-Quiz/static/js/runtime-main.57647f01.js"
+  }
+]);
